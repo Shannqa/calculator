f6e69d6e9f249b529aa5770b1b8d18ed9f32700b
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-11 21:29:42 +0100

    Add graphic display of input and  addition functionality for 2 numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
+const display = document.querySelector('.display');
+
+let displayValueZero = 0;
+display.textContent = displayValueZero;
+let displayValue = ''
+let storedValue1 = '';
+let storedValue2 = '';
+let operator;
+
+const buttonNumber = document.querySelectorAll('.button-number');
+buttonNumber.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (operator === undefined) {
+      storedValue1 = storedValue1 + button.textContent;
+      displayValue = displayValue + button.textContent;
+    } else if (operator !== undefined) {
+      storedValue2 = storedValue2 + button.textContent;
+      displayValue = displayValue + button.textContent;
+    }
+    display.textContent = displayValue;
+  })
+});
+
+const buttonPlus = document.querySelector('.button-plus');
+buttonPlus.addEventListener('click', () => {
+  operator = '+';
+  displayValue = displayValue + ' + ';
+  display.textContent = displayValue;
+});
+
+
+const buttonEquals = document.querySelector('.button-equals');
+buttonEquals.addEventListener('click', () => {
+  display.textContent = operate (operator, storedValue1, storedValue2);
+});
+
 function add (value1, value2) {
-  let result = value1 + value2;
+  let result = parseInt(value1) + parseInt(value2);
   return result;
 }
 
